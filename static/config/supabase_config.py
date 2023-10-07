4937f6863679e89cc16b5caa2bb5291498177e46
Fanfaster01 <programaionvdsl@gmail.com>
2023-10-06 22:43:37 -0400

    feat: Inicio

---
+++ static/config/supabase_config.py
@@ -0,0 +1,6 @@
+from supabase import create_client, Client
+
+url: str = "https://ollwqnmujknqabwbeuvt.supabase.co"
+key: str = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9sbHdxbm11amtucWFid2JldXZ0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTY2MzcxMDUsImV4cCI6MjAxMjIxMzEwNX0.xvKTNkJ2ODiLo9HA9xN9lwunhQvdlwqCoJjugU5UrMQ"
+supabase: Client = create_client(url, key)
+
